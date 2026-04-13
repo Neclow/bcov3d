0d316d1bc06b64061020442b3be9644308b22938
Neclow <neilclow24@gmail.com>
2026-04-13 13:35:46 +0000

    chore: fix post-install R dep script

--- scripts/install_latex.sh
+++
@@ -1,3 +0,0 @@
-sudo apt install texlive-latex-base
-sudo apt install texlive-latex-recommended texlive-fonts-recommended
-sudo apt install texlive-latex-extra cm-super dvipng
--- scripts/post_install.sh
+++ scripts/post_install.sh
@@ -1,11 +1,9 @@
 #!/bin/bash
 
 # Install extra R packages if missing
-Rscript -e 'if (!requireNamespace("envalysis", quietly = TRUE)) \
-install.packages("envalysis", repos="https://cloud.r-project.org")'
+Rscript -e 'if (!requireNamespace("envalysis", quietly = TRUE)) install.packages("envalysis", repos="https://cloud.r-project.org")'
 
-Rscript -e 'if (!requireNamespace("brms", quietly = TRUE)) \
-install.packages("brms")'
+Rscript -e 'if (!requireNamespace("brms", quietly = TRUE)) install.packages("brms", repos="https://cloud.r-project.org")'
 
 # Initialize and update submodule recursively
 git submodule update --init --recursive extern/famsa3di
